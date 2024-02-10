604de78c8f84e71769aa6f5baac0333fc8e7a52a
era-tech=420 <eratech420@gmail.com>
2024-02-10 18:15:59 +0500

    gridfs added

--- index.js
+++ index.js
@@ -8,11 +8,38 @@ const productRouter = require("./routes/product")
 const categoryRouter = require('./routes/category')
 const paymentRouter = require('./routes/payment')
 const ordersRouter = require('./routes/orders')
+const Grid = require('gridfs-stream');
+const mongoose = require('mongoose')
 const app = express()
 
-
 // static files
-app.use("/uploads",express.static('uploads'))
+// app.use("/uploads",express.static('uploads'))
+
+
+// Initialize gridfs-stream with mongoose connection
+Grid.mongo = mongoose.mongo;
+let gfs, gridFSBucket;
+// mongodb connection
+connect_mongodb().then(() => {
+    gfs = Grid(mongoose.connection.db, mongoose.mongo);
+    gfs.collection('uploads');
+    gridFSBucket = new mongoose.mongo.GridFSBucket(mongoose.connection, {
+        bucketName: "uploads",
+    });
+    console.log("gridFS Initialized")
+})
+
+app.get("/uploads/:filename", async (req, res) => {
+    try {
+        const file = await gfs.files.findOne({ filename: req.params.filename });
+        res.setHeader("content-type", file.contentType);
+        const readStream = gridFSBucket.openDownloadStream(new mongoose.Types.ObjectId(file._id));
+        readStream.pipe(res);
+    } catch (error) {
+        console.log("Error: ", error)
+        res.send("not found");
+    }
+});
 
 // cors handling
 app.use(cors())
@@ -39,9 +66,6 @@ app.all("*", (req, res) => {
         "message":"route not found."
     })
 })
-
-// mongodb connection
-connect_mongodb()
 
 // up the server
 app.listen(process.env.PORT, ()=>{
--- routes/product.js
+++ routes/product.js
@@ -2,6 +2,7 @@ const express = require('express')
 const productRouter = express.Router()
 const checkTokenMiddleware = require('../middlewares/checkToken')
 const validateProductCreateRequest = require("../requests/productCreateRequest")
+const {GridFsStorage} = require('multer-gridfs-storage');
 
 // configuring multer
 const multer  = require('multer')
@@ -11,17 +12,32 @@ const { deleteProduct } = require("../controllers/ProductController")
 const validateProductDeleteRequest = require("../requests/productDeleteRequest")
 const checkProductMiddleware = require("../middlewares/checkProduct")
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads')
-    },
-    filename: function (req, file, cb) {
-      console.log(file)
-      const extension = file.mimetype.split("/")[1]
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix + '.' + extension)
-    }
-})
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, 'uploads')
+//     },
+//     filename: function (req, file, cb) {
+//       console.log(file)
+//       const extension = file.mimetype.split("/")[1]
+//       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+//       cb(null, file.fieldname + '-' + uniqueSuffix + '.' + extension)
+//     }
+// })
+
+// Set up GridFS storage engine
+const storage = new GridFsStorage({
+  url: process.env.MONGO_CONNECTION_URI,
+  options: { useNewUrlParser: true, useUnifiedTopology: true },
+  file: (req, file) => {
+    const extension = file.mimetype.split("/")[1]
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    const filename = file.fieldname + '-' + uniqueSuffix + '.' + extension
+    return {
+      bucketName: 'uploads',  // Bucket name in MongoDB
+      filename: filename  // Use original file name as the stored file name
+    };
+  }
+});
 
 const upload = multer({ storage: storage })
 
